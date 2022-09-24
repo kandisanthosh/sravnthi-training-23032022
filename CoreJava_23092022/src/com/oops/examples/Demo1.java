5c89d20afa418c3d31b4c5bbb49563927f7ca6a5
kandisanthosh <kandi.santhosh94@gmail.com>
2022-09-24 12:13:58 +0530

    24092022 - 30 minuts

---
+++ CoreJava_23092022/src/com/oops/examples/Demo1.java
@@ -0,0 +1,17 @@
+package com.oops.examples;
+
+public class Demo1 {
+
+	
+	public static void main(String[] args) {
+		
+		Customer customer=new Customer();
+		
+		customer.setAge("12");
+		customer.setGender("Male");
+		customer.setName("santhosh");
+		
+		System.out.print(customer);
+		
+	}
+}
---
+++ CoreJava_23092022/src/com/oops/examples/Employee.java
@@ -0,0 +1,22 @@
+package com.oops.examples;
+
+public class Employee extends Object {
+	
+	public static void main(String[] args) {
+		
+		
+		Employee employee=new Employee();
+
+		Employee employee1=new Employee();
+
+		Employee employee2=new Employee();
+
+		Employee employee3=new Employee();
+
+		Employee employee4=new Employee();
+	
+		
+	}
+	
+
+}
